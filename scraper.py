afb27a8aba6bf47ea109f2c1be16e27c1def8288
fcagnola <fede.cagnola@gmail.com>
2021-04-17 20:15:16 +0200

    update class, argparsing and type hints

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import argparse
 import requests_html
 from rich.console import Console
 
-logging.basicConfig(level=logging.CRITICAL)
+logging.basicConfig(level=logging.DEBUG)
 
 rich_console = Console()
 
@@ -16,45 +16,82 @@ class TreccaniScraper:
     BASE_URL = "https://www.treccani.it/vocabolario"
 
     def __init__(self, word):
+        '''
+        Each instance is assigned an HTML session and a word
+        both of these form the request url
+        '''
         self.sesh = requests_html.HTMLSession()
         self.word = word
+        self.definitions = []
         self.SEARCH_URL = f"{self.BASE_URL}/ricerca/{self.word}/"
-        logging.debug(f"Instantiated the object, word is: {word}")
+        logging.debug(f" Instantiated the object, word is: {word}")
 
-    def get_possibilities(self) -> set:
+    def find_pages(self) -> list:
+        '''
+        Each word can have multiple pages on Treccani:
+        this method collects all pages which contain significant meanings
+        '''
         p = self.sesh.get(self.SEARCH_URL)
         css_selector = f"a[href*=\/vocabolario\/{self.word}]"
-        word_meanings = set()
+        word_meanings = list()
         for i in p.html.find(css_selector):
+            logging.debug(f" found new page address in this element: {i}")
             href = i.attrs['href'].strip('/').split('/')[-1]
-            if "%28" not in href and "%29" not in href:
-                word_meanings.add(href)
+            if "%28" not in href and "%29" not in href: # avoid parenthesised urls (usually for non-vocabulary results)
+                if href not in word_meanings:
+                    word_meanings.append(href)
         self.word_meanings = word_meanings
-        logging.debug("Found %s pages. Looking for defintions...",
-                      len(word_meanings))
+        logging.debug(f" Found {len(word_meanings)} pages. Looking for defintions...")
         return word_meanings
 
-    def get_definitions(self) -> set:
-        text = set()
-        for i in self.word_meanings:
-            logging.debug(f"New page to scrape, meaning: {i}")
-            meaning = i
+    def scrape_definitions(self) -> list:
+        '''
+        This method scrapes the paragraphs containing definitions from a list 
+        of partial URLs like {"word1", "word2"}
+        '''
+        text = list()
+        for meaning in self.word_meanings:
+            logging.debug(f" New page to scrape: {meaning}")
             MEANING_URL = f"{self.BASE_URL}/{meaning}/"
             page = self.sesh.get(MEANING_URL)
+            
+            # Find paragraph(s) with the word's definitions 
             selector = f"div.text.spiega > p"
+            print(page.html.find(selector))
             for i in page.html.find(selector):
-                text.add(i.text)
+                logging.debug(f" Div-text found in the page")
+                txt = i.text
+                digit_pattern = re.compile(r"(?:\s(\d)\.\s[a-z]\.\s)|(?:\s(\d)\.\s)|(?:\.\s(–))") # digit + letter or only digit
+                letter_pattern = re.compile(r"(?<!\s[a-z])[:.]\s([a-z])\.\s(?![a-z]\.)") # only letter pattern preceded by : or . not repeated before/after  
+                txt = re.sub(digit_pattern, r"&&m&&", txt)  # meanings become this string
+                txt = re.sub(letter_pattern, r"££s££", txt) # sub-meanings become this string
+                text.append(txt)
+        logging.debug(f" Found {len(text)} paragraphs.")
+        self.raw_text = text
         return text
 
-    def find_definitions(self, text) -> list:
-        res = []
-        logging.debug(f"text is: \n{text}")
-        pattern = re.compile(r"(?:–\s)(?:1\.\s)?(?:[a-z]\.\s)?(.*?)(?:[:;\(\)])")
-        results = pattern.finditer(text)
-        for match in results:
-            def_string = match.group(1)
-            res.append(def_string)
-        return res
+    def split_text_definition(self, text):
+        '''
+        This method does the heavy lifting of separating the useful text from
+        the raw scraped definition: as of now it only takes the first sentence
+        of each 'meaning' and puts it in the definitions list'''
+        pattern = re.compile(r"\s?&&.*?&&|££.*?££") # &&x&& OR ££x££ 
+        def_list = re.split(pattern, text)
+        for i in def_list[1:2]:
+            stops = re.compile(r"[;:.(-](?!,)")
+            l = re.split(stops, i)
+            # in case the sentence was split on . and the search word was the same letter, add the remaining of the word
+            if len(l[0].split(" ")[-1]) == 1 and self.word[0] == l[0].split(" ")[-1]:
+                l[0] += self.word[1:]
+            self.definitions.append(l[0])
+        return
+
+    def scrape(self) -> list: # main function, calls the intermediate methods and returns the result
+        self.find_pages()
+        self.scrape_definitions()
+        for text in self.raw_text:
+            self.split_text_definition(text)
+        return self.definitions
 
 
 # -------------------------------------------------------------------------------------------------------------
@@ -62,15 +99,14 @@ class TreccaniScraper:
 
 def main():
     user_input = sys.argv[1]
-    logging.info("Looking for '%s'", user_input)
-    rich_console.print(f"\n[cyan bold underline]Searching for word '{user_input}'\n")
+    logging.info(f"Looking for '{user_input}'")
+    rich_console.print(f"\n[cyan bold underline]Searching for word '{user_input}...'\n")
     scraper = TreccaniScraper(user_input)
-    scraper.get_possibilities()
-    definitions_txt = scraper.get_definitions()
-    for text in definitions_txt:
-        rich_console.rule(f"\n[cyan bold]{user_input.title()}:\n")
-        rich_console.print("\n".join(scraper.find_definitions(text)))
-
+    scraper.scrape()
+    for definition in scraper.definitions:
+        rich_console.rule(scraper.word.title())
+        rich_console.print(definition)
+        
 
 main()
 
